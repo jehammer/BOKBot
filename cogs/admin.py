fcc25fc45e34f9882c4b745e0d44be30b0e2728f
Joshua <jebelfor@gmail.com>
2022-05-13 21:15:24 -0500

    Admin Cog Unprivatized

--- cogs/admin.py
+++ cogs/admin.py
@@ -9,19 +9,18 @@ class Admin(commands.Cog, name="Admin"):
         self.bot = bot
         logging.info("Admin cog loaded")
 
-        # Needs to be bot.command() here
-        @bot.command()
-        async def servers(ctx):
-            """Check the servers the bot is active in, Owner only"""
-            if ctx.message.author.id == 212634819190849536:
-                try:
-                    all_servers = list(bot.guilds)
-                    await ctx.send(f"Connected on {str(len(all_servers))} servers:")
-                    await ctx.send('\n'.join(guild.name for guild in all_servers))
-                except Exception as e:
-                    logging.error("Server Check Error: " + str(e))
-            else:
-                await ctx.send("You do not have permission to do that.")
+    @commands.command()
+    async def servers(self, ctx: commands.context):
+        """Check the servers the bot is active in, Owner only"""
+        if ctx.message.author.id == 212634819190849536:
+            try:
+                all_servers = list(self.bot.guilds)
+                await ctx.send(f"Connected on {str(len(all_servers))} servers:")
+                await ctx.send('\n'.join(guild.name for guild in all_servers))
+            except Exception as e:
+                logging.error("Server Check Error: " + str(e))
+        else:
+            await ctx.send("You do not have permission to do that.")
 
 
 def setup(bot: commands.Bot):
