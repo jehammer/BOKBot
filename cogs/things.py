e879ab23accea2892138cc28fbb48261b59174c1
Joshua <jebelfor@gmail.com>
2022-06-13 16:45:02 -0500

    Unbiased image

--- cogs/things.py
+++ cogs/things.py
@@ -533,6 +533,15 @@ Goodnight BOK
             await ctx.send("Unable to send the stuff")
             logging.error(f"R34 error: {str(e)}")
 
+    @commands.command(aliases=["unbi", "unbiased"])
+    async def unbiased(self, ctx: commands.Context):
+        """Something Totally Unbiased"""
+        try:
+            await ctx.send("https://media.discordapp.net/attachments/911730032286785536/986022565560602654/unknown.png")
+        except Exception as e:
+            await ctx.send("Unable to send the image")
+            logging.error(f"Unbiased error: {str(e)}")
+
 #    @tasks.loop(time=datetime.time(12, 0, 0, 0))
 #    async def arma_reminder(self, bot):
 #        """An automated task to remind Arma to do stuff"""
