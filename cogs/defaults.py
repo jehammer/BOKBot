9cfd425fedf5d3b3a5971dfd77a7f8d2f180048f
Joshua Hammer <jebelfor@gmail.com>
2023-02-09 23:45:20 -0600

    Defaults typo fix

--- cogs/defaults.py
+++ cogs/defaults.py
@@ -64,7 +64,7 @@ class Defaults(commands.Cog, name="Defaults"):
 
     @commands.command(name="setdef")
     async def admin_set_default_role(self, ctx: commands.Context, m: discord.Member, role="check"):
-        """Officer manually assigndefault role | `!setdef [@user] [role]`"""
+        """Officer manually assign default role | `!setdef [@user] [role]`"""
         try:
             officer = discord.utils.get(ctx.message.author.guild.roles, name=self.bot.config['roles']['admin'])
             user = ctx.message.author
