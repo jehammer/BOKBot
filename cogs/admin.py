0623f8c95575dd1de87dc24da0d3cb0e600dffb7
Joshua Hammer <jebelfor@gmail.com>
2023-04-27 21:27:55 -0500

    Admin Perm Decorators

--- cogs/admin.py
+++ cogs/admin.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 import logging
 import asyncio
+import decor.perms as permissions
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s: %(message)s')
 
@@ -13,6 +14,7 @@ class Admin(commands.Cog, name="Admin"):
         self.bot = bot
 
     @commands.command(name="servers", hidden=True)
+    @permissions.creator_only()
     async def servers(self, ctx: commands.Context):
         """Check the servers the bot is active in, Owner only"""
         if ctx.message.author.id == self.bot.config["creator"]:
@@ -26,50 +28,42 @@ class Admin(commands.Cog, name="Admin"):
             await ctx.send("You do not have permission to do that.")
 
     @commands.command(name="shutdown", hidden=True)
+    @permissions.creator_only()
     async def shutdown(self, ctx: commands.Context):
         """Shut down the bot, Owner only"""
-        if ctx.message.author.id == self.bot.config["creator"]:
-            try:
-                await self.bot.close()
-            except Exception as e:
-                logging.error(f"Shutdown Error: {str(e)}")
-        else:
-            await ctx.send("You do not have permission to do that.")
+        try:
+            await self.bot.close()
+        except Exception as e:
+            logging.error(f"Shutdown Error: {str(e)}")
 
     @commands.command(name="getarma")
+    @permissions.has_officer()
     async def get_arma(self, ctx: commands.Context):
         """Gets Arma with a series of DMs and pings in case he forgets again"""
-        role = discord.utils.get(ctx.message.author.guild.roles, name=self.bot.config["roles"]["admin"])
-        user = ctx.message.author
-        if user in role.members:
-            try:
-                arma = ctx.message.guild.get_member(self.bot.config["arma"])
-                if arma:
-                    for i in range(4):
-                        await arma.send("It is time for your regularly scheduled event")
-                        await ctx.send(arma.mention + " it is time for you to get on!")
-                        await asyncio.sleep(.5)
+        try:
+            arma = ctx.message.guild.get_member(self.bot.config["arma"])
+            if arma:
+                for i in range(4):
+                    await arma.send("It is time for your regularly scheduled event")
+                    await ctx.send(arma.mention + " it is time for you to get on!")
+                    await asyncio.sleep(.5)
 
-                else:
-                    await ctx.send("Cannot find Arma")
-            except Exception as e:
-                await ctx.send("I cannot call Arma")
-                logging.error(f"Call Arma error: {str(e)}")
-        else:
-            await ctx.send("You do not have permission to use this command.")
+            else:
+                await ctx.send("Cannot find Arma")
+        except Exception as e:
+            await ctx.send("I cannot call Arma")
+            logging.error(f"Call Arma error: {str(e)}")
 
     @commands.command(name="sr", hidden=True)
+    @permissions.creator_only()
     async def send_message_into_chat(self, ctx: commands.Context):
         """Just a fun little thing"""
         try:
-            if ctx.message.author.id == self.bot.config["creator"]:
-                msg = ctx.message.content
-                msg = msg.split(" ", 2)
-                guild = self.bot.get_guild(self.bot.config["guild"])
-                channel = guild.get_channel(int(msg[1]))
-                await channel.send(msg[2])
-            else:
-                await ctx.send(f"You do not have permission to do this.")
+            msg = ctx.message.content
+            msg = msg.split(" ", 2)
+            guild = self.bot.get_guild(self.bot.config["guild"])
+            channel = guild.get_channel(int(msg[1]))
+            await channel.send(msg[2])
         except Exception as e:
             await ctx.send("Unable to send the message")
             logging.error(f"sr error: {str(e)}")
