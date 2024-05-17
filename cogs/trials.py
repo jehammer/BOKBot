a89fee2304bfa573dbe80e2dbe0b468504d1e8e5
Joshua Hammer <jebelfor@gmail.com>
2024-05-17 00:03:48 -0500

    Roster Creation Command

--- cogs/trials.py
+++ cogs/trials.py
@@ -5,6 +5,14 @@ from discord import app_commands, Interaction
 import logging
 import asyncio
 
+# My created imports
+import decor as permissions
+from errors import *
+from modals import *
+from models import roster, count
+from services import Utilities, RosterExtended
+
+
 
 logging.basicConfig(
     level=logging.INFO, format='%(asctime)s: %(message)s',
@@ -23,5 +31,11 @@ class Trials(commands.Cog, name="Trials"):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
+
+    @app_commands.command(name='trial', description='For Raid Leads: Opens Trial Creation Modal')
+    @permissions.application_has_raid_lead()
+    async def create_roster(self, interaction: discord.Interaction) -> None:
+        user_language = Utilities.get_language(interaction.user)
+        await interaction.response.send_modal(TrialModal(None, interaction, self.bot.config, self.bot.language[user_language]))
 async def setup(bot: commands.Bot):
     await bot.add_cog(Trials(bot))
