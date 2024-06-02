5240d685fadd7d8bf282cd61a8ad9d3d185bd410
Joshua Hammer <jebelfor@gmail.com>
2024-06-02 16:22:52 -0500

    Count Data Increase and Decrease

--- models/count.py
+++ models/count.py
@@ -2,7 +2,7 @@ class Count:
     """Class to manage Count information for Run History"""
 
     def __init__(self, runs=None, trial=None, date=None, dps=None, tank=None, healer=None):
-        self.count = runs if runs is not None else "0",
+        self.count = runs if runs is not None else 0,
         self.lastTrial = trial if trial is not None else "None",
         self.lastDate = date if date is not None else "<t:0:f>",
         self.dpsRuns = dps if dps is not None else 0,
@@ -21,3 +21,20 @@ class Count:
         }
 
         return all_data
+
+    def increase_data(self, runs=0, trial=None, date=None, dps=0, tank=0, healer=0):
+        self.count += runs
+        self.lastTrial = trial if trial is not None else "None"
+        self.lastDate = date if date is not None else "<t:0:f>"
+        self.dpsRuns += dps
+        self.tankRuns += tank
+        self.healerRuns += healer
+
+    def decrease_data(self, runs=0, trial=None, date=None, dps=0, tank=0, healer=0):
+        self.count -= runs
+        self.lastTrial = trial if trial is not None else "None"
+        self.lastDate = date if date is not None else "<t:0:f>"
+        self.dpsRuns -= dps
+        self.tankRuns -= tank
+        self.healerRuns -= healer
+
