9a52f051764e9b91f424753c1198b1c53d60420c
Joshua Hammer <jebelfor@gmail.com>
2024-07-20 11:40:44 -0500

    Position Refactor

--- bot.py
+++ bot.py
@@ -1,30 +1,113 @@
 #!/usr/bin/python3
 import asyncio
-import discord
 from discord.ext import commands
-from discord import app_commands
-import os
-import logging
-import yaml
+from discord import app_commands, Intents, Interaction, Game
 from datetime import datetime
+import logging
 import shutil
+import yaml
+import os
 import re
+
+# Bot-Specific Imports
 from errors import *
+from services import Utilities
 
 
-intents = discord.Intents.all()
+intents = Intents.all()
 intents.members = True
-bot = commands.Bot(command_prefix="!", case_insensitive=True, intents=intents)
-bot.remove_command("help")  # the help.py cog will replace the default command
-log_name = "log.log"
+bot = commands.Bot(command_prefix='!', case_insensitive=True, intents=intents)
+bot.remove_command('help')  # the help.py cog will replace the default command
+log_name = 'log.log'
 
-role = None
-ranks = None
-poons = None
-other = None
+# Value Loaders
+def load_configurations():
+    full_config = {}
+    directory = os.path.join(os.getcwd(), 'config')
+    if not os.path.exists(directory):
+        logging.error(f"The directory {directory} does not exist.")
+        return
+    for filename in os.listdir(directory): # Go through all the config files but ignore templates
+        if filename.endswith('.yaml') or filename.endswith('.yml'):
+            if not filename.lower().startswith('template'):
+                file_path = os.path.join(directory, filename)
 
+                # Open and read the YAML file
+                with open(file_path, 'r') as file:
+                    file_content = yaml.safe_load(file)
 
-# Events
+                    # Ensure the file content is a dictionary
+                    if isinstance(file_content, dict):
+                        full_config.update(file_content)
+                    else:
+                        logging.warning(f"Load Configuration Warning: {filename} is improperly formatted "
+                                        f"and doesn't make a dictionary")
+    return full_config
+
+def load_languages():
+    """Function to load all the Language options for BOKBot"""
+    languages = {}
+    for root, _, files in os.walk("languages"):
+        for file in files:
+            if file.endswith(".yaml"):
+                filepath = os.path.join(root, file)
+                language = os.path.basename(root)
+                section = os.path.splitext(file)[0]
+
+                if language not in languages:
+                    languages[language] = {}
+
+                with open(filepath, "r", encoding="utf-8") as f:
+                    data = yaml.safe_load(f)
+                    languages[language][section] = data
+    return languages
+
+async def load_cogs():
+    """Load cogs from the cogs folder"""
+    for filename in os.listdir('cogs'):
+        if filename.endswith('.py') and not filename.startswith('_'):
+            try:
+                await bot.load_extension(f"cogs.{filename[:-3]}")
+                logging.info(f"Successfully loaded {filename}")
+
+            except Exception as e:
+                logging.info(f"Failed to load {filename}")
+                logging.error(f"cog load error: {str(e)}")
+
+async def startup_logging():
+    """Checks if there are logs from a bad shutdown"""
+    try:
+        date = datetime.now().strftime('%m-%d-%Y')
+        if os.path.exists(log_name):
+            file_name = f"log-{date}-crash.log"
+            os.makedirs("logs", exist_ok=True)
+            version = 1
+            while os.path.exists(os.path.join('logs', file_name)):
+                base_name, extension = os.path.splitext(file_name)
+                base_name = re.sub(r'\(\d{1,2}\)', '', base_name)
+                file_name = f"{base_name}({version}){extension}"
+                version += 1
+            path = os.path.join('logs', file_name)
+            # Quick fix for the handlers causing issues with moving the logs, so close them quickly before starting again.
+            for handler in logging.root.handlers[:]:
+                logging.root.removeHandler(handler)
+                handler.close()
+            shutil.move(log_name, path)
+        time = datetime.now().strftime('%I:%M:%S %p')
+        with open(log_name, 'w') as file:
+            file.write(f"Bot started at: {time} on {date}\n\n")
+
+        logging.basicConfig(
+            level=logging.INFO, format='%(asctime)s: %(message)s',
+            handlers=[
+                logging.FileHandler('log.log', mode='a'),
+                logging.StreamHandler()
+            ])  # , datefmt="%Y-%m-%d %H:%M:%S")
+
+    except Exception as e:
+        logging.error(f"I was unable to set up the new logging information: {str(e)}")
+
+# Events and Errors
 @bot.event
 async def on_command_error(ctx, error):
     if isinstance(error, commands.CommandNotFound):
@@ -47,7 +130,7 @@ async def on_command_error(ctx, error):
         await ctx.send("Unable to complete the command. I am not sure which error was thrown.")
         logging.error(f"Generic Error: {str(error)}")
 
-async def on_tree_error(interaction: discord.Interaction, error: app_commands.AppCommandError):
+async def on_tree_error(interaction: Interaction, error: app_commands.AppCommandError):
     if isinstance(error, app_commands.MissingPermissions):
         return await interaction.response.send_message(f"You're missing permissions to use that")
     elif isinstance(error, app_commands.MissingRole):
@@ -55,90 +138,19 @@ async def on_tree_error(interaction: discord.Interaction, error: app_commands.Ap
     else:
         await interaction.response.send_message(f"Some weird error is being thrown. Not sure what it is")
         logging.error(f"{str(error)}")
-
 bot.tree.on_error = on_tree_error
 
-def load_configurations():
-    with open("config.yaml", 'r') as stream:
-        data_loaded = yaml.safe_load(stream)
-    return data_loaded
-
-def load_languages():
-    """Function to load all the Language options for BOKBot"""
-    languages = {}
-    for root, _, files in os.walk("languages"):
-        for file in files:
-            if file.endswith(".yaml"):
-                filepath = os.path.join(root, file)
-                language = os.path.basename(root)
-                section = os.path.splitext(file)[0]
-
-                if language not in languages:
-                    languages[language] = {}
-
-                with open(filepath, "r", encoding="utf-8") as f:
-                    data = yaml.safe_load(f)
-                    languages[language][section] = data
-
-    return languages
-
-
-
-async def change_playing():
-    await bot.change_presence(activity=discord.Game(name=bot.config['presence_message']))
+async def set_playing():
+    await bot.change_presence(activity=Game(name=bot.config['presence_message']))
     logging.info(f"Status has been set")
-
-
-async def load_cogs():
-    """Load cogs from the cogs folder"""
-    for filename in os.listdir("cogs"):
-        if filename.endswith(".py") and not filename.startswith("_"):
-            try:
-                await bot.load_extension(f"cogs.{filename[:-3]}")
-                logging.info(f"Successfully loaded {filename}")
-
-            except Exception as e:
-                logging.info(f"Failed to load {filename}")
-                logging.error(f"cog load error: {str(e)}")
-
-async def startup_logging():
-    """Checks if there is logs from a bad shutdown"""
-    try:
-        date = datetime.now().strftime("%m-%d-%Y")
-        if os.path.exists(log_name):
-            file_name = f"log-{date}-crash.log"
-            os.makedirs("logs", exist_ok=True)
-            version = 1
-            while os.path.exists(os.path.join("logs", file_name)):
-                base_name, extension = os.path.splitext(file_name)
-                base_name = re.sub(r'\(\d{1,2}\)', '', base_name)
-                file_name = f"{base_name}({version}){extension}"
-                version += 1
-            path = os.path.join("logs", file_name)
-            shutil.move(log_name, path)
-        time = datetime.now().strftime("%I:%M:%S %p")
-        with open(log_name, "w") as file:
-            file.write(f"Bot started at: {time} on {date}\n\n")
-
-        logging.basicConfig(
-            level=logging.INFO, format='%(asctime)s: %(message)s',
-            handlers=[
-                logging.FileHandler('log.log', mode='a'),
-                logging.StreamHandler()
-            ])  # , datefmt="%Y-%m-%d %H:%M:%S")
-
-    except Exception as e:
-        logging.error(f"I was unable to set up the new logging information: {str(e)}")
-
 
 @bot.event
 async def on_ready():
     logging.info(f"Logged in as: {bot.user.name}")
-    await gather_roles(bot.get_guild(bot.config["guild"]), bot.config)
-    await change_playing()
-    logging.info("Bot is ready for use")
-    logging.info("Sending out load_on_ready Event")
-    bot.dispatch("load_on_ready", bot)
+    await set_playing()
+    logging.info('Bot is ready for use')
+    logging.info('Sending out load_on_ready Event')
+    bot.dispatch('load_on_ready', bot)
 
 async def main():
     async with bot:
