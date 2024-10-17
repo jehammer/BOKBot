6dbc2c6e34e660a31095f5d88e2124cca4cb9f9c
Joshua Hammer <jebelfor@gmail.com>
2024-10-17 00:27:54 -0500

    Rank Model

---
+++ models/rank.py
@@ -0,0 +1,27 @@
+class Rank:
+    """A class object to store and manage ranking information"""
+
+    def __init__(self, count, last_called, lowest, highest, doubles, singles, six_nine, four_twenty, boob):
+        self.count = count
+        self.last_called = last_called
+        self.lowest = lowest
+        self.highest = highest
+        self.doubles = doubles
+        self.singles = singles
+        self.six_nine = six_nine
+        self.four_twenty = four_twenty
+        self.boob = boob
+
+    def get_data(self):
+        all_data = {
+            "count": self.count,
+            "last_called": self.last_called,
+            "lowest": self.lowest,
+            "highest": self.highest,
+            "doubles": self.doubles,
+            "singles": self.singles,
+            "six_nine": self.six_nine,
+            "four_twenty": self.four_twenty,
+            "boob": self.boob
+        }
+        return all_data
