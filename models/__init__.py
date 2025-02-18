1598844df9a469e7c4de363b2c39a22d7a839502
Joshua Hammer <jebelfor@gmail.com>
2025-02-17 23:02:21 -0600

    Rank Model

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,6 @@
 from .roster import Roster
 from .count import Count
 from .leader_check import LeaderCheck
+from .rank import Rank
 
-__all__ = ['Roster', 'Count', 'LeaderCheck']
+__all__ = ['Roster', 'Count', 'LeaderCheck', 'Rank']
--- models/rank.py
+++ models/rank.py
@@ -1,7 +1,7 @@
 class Rank:
     """A class object to store and manage ranking information"""
 
-    def __init__(self, count, last_called, lowest, highest, doubles, singles, six_nine, four_twenty, boob):
+    def __init__(self, count, last_called, lowest, highest, doubles, singles, six_nine, four_twenty, boob, pie):
         self.count = count
         self.last_called = last_called
         self.lowest = lowest
@@ -11,6 +11,7 @@ class Rank:
         self.six_nine = six_nine
         self.four_twenty = four_twenty
         self.boob = boob
+        self.pie = pie
 
     def get_data(self):
         all_data = {
@@ -22,6 +23,7 @@ class Rank:
             "singles": self.singles,
             "six_nine": self.six_nine,
             "four_twenty": self.four_twenty,
-            "boob": self.boob
+            "boob": self.boob,
+            "pie": self.pie
         }
         return all_data
