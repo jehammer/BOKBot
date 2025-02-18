007a1806e74f4492d0e8cb33830b4a5e2057cbfa
Joshua Hammer <jebelfor@gmail.com>
2025-02-17 23:57:12 -0600

    Created Migration Cog Placeholder

---
+++ cogs/_migration.py
@@ -0,0 +1,3 @@
+# TODO: Replace Singles with Samsies on Ranking migration
+# TODO: Check for if anyone is not longer in the Discord when migrating data.
+# TODO: When migrating rosters create the new ping role.
--- models/rank.py
+++ models/rank.py
@@ -1,7 +1,8 @@
 class Rank:
     """A class object to store and manage ranking information"""
 
-    def __init__(self, count, last_called, lowest, highest, doubles, singles, six_nine, four_twenty, boob, pie):
+    def __init__(self, count=0, last_called=None, lowest=1000000, highest=0, doubles=0,
+                 singles=0, six_nine=0, four_twenty=0, boob=0, pie=0, samsies=0):
         self.count = count
         self.last_called = last_called
         self.lowest = lowest
@@ -12,6 +13,7 @@ class Rank:
         self.four_twenty = four_twenty
         self.boob = boob
         self.pie = pie
+        self.samsies = samsies
 
     def get_data(self):
         all_data = {
@@ -24,6 +26,7 @@ class Rank:
             "six_nine": self.six_nine,
             "four_twenty": self.four_twenty,
             "boob": self.boob,
-            "pie": self.pie
+            "pie": self.pie,
+            "samsies": self.samsies
         }
         return all_data
