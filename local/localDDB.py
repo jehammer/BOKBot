28d7743b01fc788cc60e39f6450fbc910579d744
Joshua Hammer <jebelfor@gmail.com>
2025-02-23 23:59:27 -0600

    LocalDDB Formatting

--- local/localDDB.py
+++ local/localDDB.py
@@ -17,14 +17,13 @@ logging.basicConfig(
         logging.StreamHandler()
     ])  # , datefmt="%Y-%m-%d %H:%M:%S")
 
-
-
 ddb_local_endpoint = 'http://localhost:8000'
 region = 'us-east-2'
 access_key = 'dummy'
 secret_key = 'dummy'
 dynamodb = boto3.client('dynamodb', endpoint_url=ddb_local_endpoint,
                         region_name=region, aws_access_key_id=access_key, aws_secret_access_key=secret_key)
+
 
 def setup_misc_table():
     table_name = 'Misc'
@@ -57,6 +56,7 @@ def setup_misc_table():
     except Exception as e:
         logging.error(f"Misc Table setup error: {str(e)}")
 
+
 def setup_roster_table():
     table_name = 'Rosters'
     key_schema = [
@@ -87,6 +87,7 @@ def setup_roster_table():
         logging.info(f"Rosters Table already exists.")
     except Exception as e:
         logging.error(f"Rosters Table setup error: {str(e)}")
+
 
 def setup_count_table():
     table_name = 'Count'
@@ -119,6 +120,7 @@ def setup_count_table():
     except Exception as e:
         logging.error(f"Count Table setup error: {str(e)}")
 
+
 def setup_default_table():
     table_name = 'Defaults'
     key_schema = [
@@ -149,6 +151,7 @@ def setup_default_table():
         logging.info(f"Defaults Table already exists.")
     except Exception as e:
         logging.error(f"Defaults Table setup error: {str(e)}")
+
 
 def setup_ranks_table():
     table_name = 'Ranks'
@@ -181,7 +184,8 @@ def setup_ranks_table():
     except Exception as e:
         logging.error(f"Ranks Table setup error: {str(e)}")
 
-def setup_birthdays_table(): #TODO: Revisit this, this is just a setup for now.
+
+def setup_birthdays_table():  # TODO: Revisit this, this is just a setup for now.
     table_name = 'Birthdays'
     key_schema = [
         {
@@ -211,6 +215,7 @@ def setup_birthdays_table(): #TODO: Revisit this, this is just a setup for now.
         logging.info(f"Birthdays Table already exists.")
     except Exception as e:
         logging.error(f"Birthdays Table setup error: {str(e)}")
+
 
 def main():
     setup_misc_table()
