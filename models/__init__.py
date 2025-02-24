b471dce4a3076cf5b5d64ca6dbf34ff394a5191f
Joshua Hammer <jebelfor@gmail.com>
2025-02-23 23:16:41 -0600

    Remove leader_check model

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,5 @@
 from .roster import Roster
 from .count import Count
-from .leader_check import LeaderCheck
 from .rank import Rank
 
-__all__ = ['Roster', 'Count', 'LeaderCheck', 'Rank']
+__all__ = ['Roster', 'Count', 'Rank']
