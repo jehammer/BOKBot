ba8aca93d3f4bd709d6e61d02b4adee32382f9c5
Joshua Hammer <jebelfor@gmail.com>
2025-06-13 00:17:52 -0500

    Delete unneeded file

--- bot/cogs/_migration.py
+++
@@ -1,3 +0,0 @@
-# TODO: Replace Singles with Samsies on Ranking migration
-# TODO: Check for if anyone is not longer in the Discord when migrating data.
-# TODO: When migrating rosters create the new ping role.
