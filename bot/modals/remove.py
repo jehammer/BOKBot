2dd39d23805e951f4d7e3d58ab02cfb6d93af8c3
Joshua Hammer <jebelfor@gmail.com>
2025-11-06 23:14:34 -0600

    Remove Modal Select Menu

--- bot/modals/remove.py
+++ bot/modals/remove.py
@@ -1,6 +1,6 @@
 from bot.models import EventRoster, Roster
-from discord.ui import Modal, TextInput
-from discord import Interaction, TextStyle
+from discord.ui import Modal, Label, Select
+from discord import Interaction, SelectOption
 from bot.services import Utilities
 import logging
 
@@ -10,7 +10,6 @@ class RemoveModal(Modal):
         self.channel_id = channel_id
         self.channel = interaction.guild.get_channel(int(self.channel_id))
         self.roster = bot.rosters[channel_id]
-        self.people = {}
         self.bot = bot
         self.ui = bot.language[user_lang]['ui']['Remove']
         self.language = bot.language[user_lang]['replies']
@@ -19,65 +18,65 @@ class RemoveModal(Modal):
         self.initialize(interaction)
 
     def initialize(self, interaction):
-        counter = 1
-        total = ""
+        counter = 0
+        total = []
         # Print out everyone and put them in a list to get from
         for i in self.roster.dps.keys():
-            self.people[counter] = i
-            total += f"{counter}: {interaction.guild.get_member(int(i)).display_name}\n"
+            guildie = interaction.guild.get_member(int(i))
+            total.append(SelectOption(label=guildie.display_name, value=i))
             counter += 1
         for i in self.roster.healers.keys():
-            self.people[counter] = i
-            total += f"{counter}: {interaction.guild.get_member(int(i)).display_name}\n"
+            guildie = interaction.guild.get_member(int(i))
+            total.append(SelectOption(label=guildie.display_name, value=i))
             counter += 1
         for i in self.roster.tanks.keys():
-            self.people[counter] = i
-            total += f"{counter}: {interaction.guild.get_member(int(i)).display_name}\n"
+            guildie = interaction.guild.get_member(int(i))
+            total.append(SelectOption(label=guildie.display_name, value=i))
             counter += 1
         for i in self.roster.backup_dps.keys():
-            self.people[counter] = i
-            total += f"{counter}: {interaction.guild.get_member(int(i)).display_name}\n"
+            guildie = interaction.guild.get_member(int(i))
+            total.append(SelectOption(label=guildie.display_name, value=i))
             counter += 1
         for i in self.roster.backup_healers.keys():
-            self.people[counter] = i
-            total += f"{counter}: {interaction.guild.get_member(int(i)).display_name}\n"
+            guildie = interaction.guild.get_member(int(i))
+            total.append(SelectOption(label=guildie.display_name, value=i))
             counter += 1
         for i in self.roster.backup_tanks.keys():
-            self.people[counter] = i
-            total += f"{counter}: {interaction.guild.get_member(int(i)).display_name}\n"
+            guildie = interaction.guild.get_member(int(i))
+            total.append(SelectOption(label=guildie.display_name, value=i))
             counter += 1
 
-        self.users = TextInput(
-            label=f"{self.channel.name}",
-            default=f"{total}",
-            style=TextStyle.long,
-            required=False
+        self.users = Label(
+            text=f"{self.ui['Label']}",
+            description=f"{self.ui['Description']}",
+            component=Select(
+                placeholder=f"{self.ui['Placeholder']}",
+                options=total,
+                max_values=counter
+            ),
         )
-        self.options = TextInput(
-            label=f"{self.ui['Label']}",
-            placeholder=f"{self.ui['Placeholder']}",
-            style=TextStyle.short,
-            required=True
-        )
+
         self.add_item(self.users)
-        self.add_item(self.options)
 
     async def on_submit(self, interaction: Interaction):
         try:
-            removed = [int(i) for i in self.options.value.split(',')]
+
+            assert isinstance(self.users.component, Select)
+            removed = self.users.component.values
             names = ''
             for i in removed:
-                if self.people[i] in self.bot.rosters[self.channel_id].dps.keys() or self.people[i] in self.bot.rosters[self.channel_id].backup_dps.keys():
-                    self.bot.rosters[self.channel_id].remove_dps(self.people[i])
-                    names += f"{interaction.guild.get_member(int(self.people[i])).display_name}\n"
-                elif (self.people[i] in self.bot.rosters[self.channel_id].healers.keys() or self.people[i] in
+                if (i in self.bot.rosters[self.channel_id].dps.keys() or i in
+                        self.bot.rosters[self.channel_id].backup_dps.keys()):
+                    self.bot.rosters[self.channel_id].remove_dps(i)
+                    names += f"{interaction.guild.get_member(int(i)).display_name}\n"
+                elif (i in self.bot.rosters[self.channel_id].healers.keys() or i in
                       self.bot.rosters[self.channel_id].backup_healers.keys()):
-                    self.bot.rosters[self.channel_id].remove_healer(self.people[i])
-                    names += f"{interaction.guild.get_member(int(self.people[i])).display_name}\n"
-                elif (self.people[i] in self.bot.rosters[self.channel_id].tanks.keys() or self.people[i] in
+                    self.bot.rosters[self.channel_id].remove_healer(i)
+                    names += f"{interaction.guild.get_member(int(i)).display_name}\n"
+                elif (i in self.bot.rosters[self.channel_id].tanks.keys() or i in
                       self.bot.rosters[self.channel_id].backup_tanks.keys()):
-                    self.bot.rosters[self.channel_id].remove_tank(self.people[i])
-                    names += f"{interaction.guild.get_member(int(self.people[i])).display_name}\n"
+                    self.bot.rosters[self.channel_id].remove_tank(i)
+                    names += f"{interaction.guild.get_member(int(i)).display_name}\n"
 
             self.bot.librarian.put_roster(self.channel_id, self.bot.rosters[self.channel_id])
 
@@ -85,10 +84,12 @@ class RemoveModal(Modal):
                                                        % (self.channel.name, names)}")
             return
         except ValueError:
-            await interaction.response.send_message(f"{Utilities.format_error(self.user_language, self.language['Remove']['NumbersOnly'])}")
+            await interaction.response.send_message(
+                f"{Utilities.format_error(self.user_language, self.language['Remove']['NumbersOnly'])}")
             return
 
     async def on_error(self, interaction: Interaction, error: Exception) -> None:
-        await interaction.response.send_message(f"{Utilities.format_error(self.user_language, self.language['Unknown'])}")
+        await interaction.response.send_message(
+            f"{Utilities.format_error(self.user_language, self.language['Unknown'])}")
         logging.error(f"Remove From Roster Error: {str(error)}")
         return
