b39d71a306651bc8ce71de7d0f05ad94f1922a09
Joshua Hammer <jebelfor@gmail.com>
2026-02-17 22:35:03 -0600

    Birthday App Command

--- bot/cogs/fun.py
+++ bot/cogs/fun.py
@@ -6,15 +6,15 @@ import time
 from discord import app_commands, Interaction, Member
 from discord.ext import commands
 
+from bot.modals import BirthdayModal
 from bot.models import Rank
 from bot.services import Utilities, EmbedFactory
 
 logging.basicConfig(
-    level=logging.INFO, format='%(asctime)s: %(message)s',
-    handlers=[
-        logging.FileHandler('log.log', mode='a'),
-        logging.StreamHandler()
-    ])  # , datefmt="%Y-%m-%d %H:%M:%S")
+    level=logging.INFO,
+    format="%(asctime)s: %(message)s",
+    handlers=[logging.FileHandler("log.log", mode="a"), logging.StreamHandler()],
+)  # , datefmt="%Y-%m-%d %H:%M:%S")
 
 
 class Fun(commands.Cog, name="Fun"):
@@ -26,78 +26,99 @@ class Fun(commands.Cog, name="Fun"):
     @commands.command(name="fishing")
     async def fishing(self, ctx: commands.Context):
         """Glub Glub"""
-        await ctx.send('https://cdn.discordapp.com/attachments/911730032286785536/1219447029688959067/FishFishes.gif')
+        await ctx.send(
+            "https://cdn.discordapp.com/attachments/911730032286785536/1219447029688959067/FishFishes.gif"
+        )
 
     @commands.command(name="jabs")
     async def jabs(self, ctx: commands.Context):
         """The Templars do be like that"""
-        await ctx.send('https://cdn.discordapp.com/attachments/911730032286785536/911837712196173824/jabs.gif')
+        await ctx.send(
+            "https://cdn.discordapp.com/attachments/911730032286785536/911837712196173824/jabs.gif"
+        )
 
     @commands.command(name="facepalm")
     async def facepalm(self, ctx: commands.Context):
         """Arma every other second"""
-        await ctx.send('https://cdn.discordapp.com/attachments/911730032286785536/912569604973404160/Facepalm.gif')
+        await ctx.send(
+            "https://cdn.discordapp.com/attachments/911730032286785536/912569604973404160/Facepalm.gif"
+        )
 
     @commands.command(name="fart")
     async def fart(self, ctx: commands.Context):
         """Explosive"""
-        await ctx.send('https://media.discordapp.net/attachments/911730032286785536/932433681992278088/Creed.gif')
+        await ctx.send(
+            "https://media.discordapp.net/attachments/911730032286785536/932433681992278088/Creed.gif"
+        )
 
     @commands.command(name="dungeons")
     async def dungeons(self, ctx: commands.Context):
         """DUNGEONS"""
-        await ctx.send('https://cdn.discordapp.com/attachments/911730032286785536/983363613425278997/dungeons.gif')
+        await ctx.send(
+            "https://cdn.discordapp.com/attachments/911730032286785536/983363613425278997/dungeons.gif"
+        )
 
     @commands.command(name="bokemon")
     async def bokemon(self, ctx: commands.Context):
         """A link to a perfect song"""
-        await ctx.send('https://youtu.be/OZrs7Blmank')
+        await ctx.send("https://youtu.be/OZrs7Blmank")
 
     @commands.command(name="thepull")
     async def thepull(self, ctx: commands.Context):
         """Drak got the thing"""
-        await ctx.send('https://youtu.be/Cnf9lRtLSYk')
+        await ctx.send("https://youtu.be/Cnf9lRtLSYk")
 
     @commands.command(name="chainz", aliases=["chains"])
     async def chainz(self, ctx: commands.Context):
         """Just tell him"""
-        await ctx.send('https://tenor.com/view/e40-tellmewhentogo-gif-21713338')
+        await ctx.send("https://tenor.com/view/e40-tellmewhentogo-gif-21713338")
 
     @commands.command(name="logz")
     async def logz(self, ctx: commands.Context):
         """Actual gif of him"""
         await ctx.send("LISTEN HERE SHITHEADS!")
-        await ctx.send('https://media.discordapp.net/attachments/911730032286785536/911730138935349308/Logz.gif')
+        await ctx.send(
+            "https://media.discordapp.net/attachments/911730032286785536/911730138935349308/Logz.gif"
+        )
 
     @commands.command(name="pizza")
     async def pizza_video(self, ctx: commands.Context):
         """Leahs favorite early morning/late night meal."""
-        await ctx.send('https://youtu.be/SMkG2FDCQ7w')
+        await ctx.send("https://youtu.be/SMkG2FDCQ7w")
 
     @commands.command(name="ec")
     async def my_ec_gif(self, ctx: commands.Context):
         """You have it!"""
-        await ctx.send('https://cdn.discordapp.com/attachments/911730032286785536/1062132264382775296/DrakadorMyEC.gif')
+        await ctx.send(
+            "https://cdn.discordapp.com/attachments/911730032286785536/1062132264382775296/DrakadorMyEC.gif"
+        )
 
     @commands.command(name="noec")
     async def no_ec_gif(self, ctx: commands.Context):
         """You don't have it!"""
-        await ctx.send('https://cdn.discordapp.com/attachments/911730032286785536/1062132263980126250/DrakadorNoEC.gif')
+        await ctx.send(
+            "https://cdn.discordapp.com/attachments/911730032286785536/1062132263980126250/DrakadorNoEC.gif"
+        )
 
     @commands.command(name="noquestionsasked")
     async def no_questions_asked_gif(self, ctx: commands.Context):
         """Absolutely none."""
-        await ctx.send('https://tenor.com/view/dont-ask-no-questions-gif-8052545')
+        await ctx.send("https://tenor.com/view/dont-ask-no-questions-gif-8052545")
 
     @commands.command(name="cover")
     async def my_bok_cover(self, ctx: commands.Context):
         """Put this over your chat when you stream!"""
         await ctx.send(
-            'https://cdn.discordapp.com/attachments/911730032286785536/1219457387640000612/BOK_Stream_Cover.png')
+            "https://cdn.discordapp.com/attachments/911730032286785536/1219457387640000612/BOK_Stream_Cover.png"
+        )
 
-    @app_commands.command(name="rank", description="See how much BOKBot approves of you today.")
+    @app_commands.command(
+        name="rank", description="See how much BOKBot approves of you today."
+    )
     @app_commands.describe(member="Discord user to rank if not yourself.")
-    async def rank_app_command(self, interaction: Interaction, member: Member = None) -> None:
+    async def rank_app_command(
+        self, interaction: Interaction, member: Member = None
+    ) -> None:
         """Ranks someone out of 10000 you ping"""
         user_language = Utilities.get_language(interaction.user)
         try:
@@ -105,14 +126,17 @@ class Fun(commands.Cog, name="Fun"):
                 member = interaction.user
             elif member.bot:
                 await interaction.response.send_message(
-                    f"{Utilities.format_error(user_language, self.bot.language[user_language]['replies']['NoBots'])}")
+                    f"{Utilities.format_error(user_language, self.bot.language[user_language]['replies']['NoBots'])}"
+                )
                 return
             user_id = member.id
             rank_data: Rank = self.bot.librarian.get_rank(user_id=user_id)
 
             if rank_data is None:
                 rank_data = Rank()
-            timestamp = int(time.mktime(datetime.datetime.now().timetuple()))  # All this just for a utc timestamp int
+            timestamp = int(
+                time.mktime(datetime.datetime.now().timetuple())
+            )  # All this just for a utc timestamp int
             ran = random.randint(1, 10000)
             rank_data.last_called = f"<t:{timestamp}:f>"
             rank_data.count += 1
@@ -138,22 +162,28 @@ class Fun(commands.Cog, name="Fun"):
                 rank_data.samsies += 1
             elif len(listed) == 4 and (listed[0] + listed[1] == listed[2] + listed[3]):
                 rank_data.doubles += 1
-            elif len(listed) == 4 and (listed[0] == listed[3] and listed[1] == listed[2]):
+            elif len(listed) == 4 and (
+                listed[0] == listed[3] and listed[1] == listed[2]
+            ):
                 rank_data.palindrome += 1
 
             self.bot.librarian.put_rank(user_id=user_id, rank_data=rank_data)
 
             await interaction.response.send_message(
-                f"{self.bot.language[user_language]['replies']['Rank']['Generated'] % (member.display_name, f'{ran}{Utilities.suffix(ran)}')}")
+                f"{self.bot.language[user_language]['replies']['Rank']['Generated'] % (member.display_name, f'{ran}{Utilities.suffix(ran)}')}"
+            )
 
         except Exception as e:
             await interaction.response.send_message(
-                f"{Utilities.format_error(user_language, self.bot.language[user_language]['replies']['Unknown'])}")
+                f"{Utilities.format_error(user_language, self.bot.language[user_language]['replies']['Unknown'])}"
+            )
             logging.error(f"Rank Command Error: {str(e)}")
 
     @app_commands.command(name="kowtow", description="Check someones Ranking records")
     @app_commands.describe(member="Discord user to check if not yourself.")
-    async def send_rank_info_app_command(self, interaction: Interaction, member: Member = None) -> None:
+    async def send_rank_info_app_command(
+        self, interaction: Interaction, member: Member = None
+    ) -> None:
         """Prints a leaderboard for your /rank uses"""
         user_language = Utilities.get_language(interaction.user)
         try:
@@ -161,22 +191,28 @@ class Fun(commands.Cog, name="Fun"):
                 member = interaction.user
             elif member.bot:
                 await interaction.response.send_message(
-                    f"{Utilities.format_error(user_language, self.bot.language[user_language]['replies']['NoBots'])}")
+                    f"{Utilities.format_error(user_language, self.bot.language[user_language]['replies']['NoBots'])}"
+                )
                 return
             user_id = member.id
             rank_data: Rank = self.bot.librarian.get_rank(user_id=user_id)
             if rank_data is None:
                 await interaction.response.send_message(
-                    f"{self.bot.language[user_language]['replies']['Rank']['NoHistory']}")
+                    f"{self.bot.language[user_language]['replies']['Rank']['NoHistory']}"
+                )
                 return
 
-            embed = EmbedFactory.create_ranking(rank=rank_data, lang=self.bot.language[user_language]['ui']['Rank'],
-                                                name=interaction.user.display_name)
+            embed = EmbedFactory.create_ranking(
+                rank=rank_data,
+                lang=self.bot.language[user_language]["ui"]["Rank"],
+                name=interaction.user.display_name,
+            )
             await interaction.response.send_message(embed=embed)
 
         except Exception as e:
             await interaction.response.send_message(
-                f"{Utilities.format_error(user_language, self.bot.language[user_language]['replies']['Unknown'])}")
+                f"{Utilities.format_error(user_language, self.bot.language[user_language]['replies']['Unknown'])}"
+            )
             logging.error(f"Kowtow Error: {str(e)}")
 
     @commands.command(name="8ball")
@@ -227,7 +263,9 @@ class Fun(commands.Cog, name="Fun"):
             case 20:
                 response = "Very doubtful. "
         if ran % 2 == 1:
-            ran = random.randint(1, 10)  # Give this a like 1 in 10 chance of showing up if the number is odd
+            ran = random.randint(
+                1, 10
+            )  # Give this a like 1 in 10 chance of showing up if the number is odd
             if ran == 2:
                 response = "Fuck off I am sleeping."
 
@@ -273,15 +311,18 @@ Goodnight BOK
 """
         await ctx.send(message)
 
-    @commands.command(name='otter')
+    @commands.command(name="otter")
     async def otter(self, ctx: commands.context):
         """An Otterful Greeting!"""
         user_language = Utilities.get_language(ctx.author)
         try:
-            await ctx.send('https://cdn.discordapp.com/attachments/911730032286785536/1443359030868447232/otterful.gif ')
+            await ctx.send(
+                "https://cdn.discordapp.com/attachments/911730032286785536/1443359030868447232/otterful.gif "
+            )
         except Exception as e:
             await ctx.send(
-                f"{Utilities.format_error(user_language, self.bot.language[user_language]['replies']['Unknown'])}")
+                f"{Utilities.format_error(user_language, self.bot.language[user_language]['replies']['Unknown'])}"
+            )
             logging.error(f"Otter Error: {str(e)}")
 
     @commands.command(name="oops")
@@ -289,21 +330,45 @@ Goodnight BOK
         """Self-explanatory"""
         user_language = Utilities.get_language(ctx.author)
         try:
-            await ctx.send('https://cdn.discordapp.com/attachments/911730032286785536/1443375553167228989/Oops.png')
+            await ctx.send(
+                "https://cdn.discordapp.com/attachments/911730032286785536/1443375553167228989/Oops.png"
+            )
         except Exception as e:
-            await ctx.send(f"{Utilities.format_error(user_language, self.bot.language[user_language]['replies']['Unknown'])}")
+            await ctx.send(
+                f"{Utilities.format_error(user_language, self.bot.language[user_language]['replies']['Unknown'])}"
+            )
             logging.error(f"Oops error: {str(e)}")
 
-    @commands.command(name='uwu')
+    @commands.command(name="uwu")
     async def lily_uwu(self, ctx: commands.Context):
         """Basically an anime girl"""
         user_language = Utilities.get_language(ctx.author)
         try:
-            await ctx.send('https://media.discordapp.net/attachments/911730032286785536/1464875517672161474/Lily.png')
+            await ctx.send(
+                "https://media.discordapp.net/attachments/911730032286785536/1464875517672161474/Lily.png"
+            )
         except Exception as e:
-            await ctx.send(f"{Utilities.format_error(user_language, self.bot.language[user_language]['replies']['Unknown'])}")
+            await ctx.send(
+                f"{Utilities.format_error(user_language, self.bot.language[user_language]['replies']['Unknown'])}"
+            )
             logging.error(f"Lily error: {str(e)}")
 
+    @app_commands.command(name="birthday", description="Set your birthday for BOKBot")
+    async def set_birthday_app_command(self, interaction: Interaction) -> None:
+        """Loads modal to set user birthday"""
+        user_language = Utilities.get_language(interaction.user)
+        try:
+            await interaction.response.send_modal(
+                BirthdayModal(
+                    interaction=interaction, bot=self.bot, user_lang=user_language
+                )
+            )
+
+        except Exception as e:
+            await interaction.response.send_message(
+                f"{Utilities.format_error(user_language, self.bot.language[user_language]['replies']['Unknown'])}"
+            )
+            logging.error(f"Birthday Error: {str(e)}")
 
 
 async def setup(bot: commands.Bot):
